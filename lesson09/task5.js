392a9534c622f47384d2e1714164f2de2ede964a
Vladimir Radanov <vladimir.radanov@gmail.com>
2026-09-22 22:53:49 +0300

    lesson09

--- lesson09/task5.js
+++ lesson09/task5.js
@@ -1,16 +1,21 @@
 const users = [
-    {name: "John Doe", email: "john.doe@example.com", age: 30},
-    {name: "Jane Smith", email: "jane.smith@example.com", age: 25},
-    {name: "Bob Johnson", email: "bob.johnson@example.com", age: 35}
+    {email: "john.doe@example.com", age: 30},
+    {name: "Jane Smith", age: 25},
+    {name: "Bob Johnson", email: "bob.johnson@example.com"}
 ]
 
 for (const user of users) {
-    console.log(`Name: ${user.name}, Email: ${user.email}, Age: ${user.age}`)
+    console.log(`Name: ${user?.name}, 
+        Email: ${user?.email}, 
+        Age: ${user?.age}`)
 }
 
 console.log ('--------------------')
 
 for (const user of users) {
    const {name, email, age} = user
-   console.log(`Name: ${name}, Email: ${email}, Age: ${age}`)
+   console.log(
+    `Name: ${user?.name}, 
+    Email: ${user?.email}, 
+    Age: ${user?.age}`)
 }
